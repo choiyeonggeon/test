a173b4f867b3af12a4f15ddf2f57f58ff82ec45c
내배캠 <as14561@naver.com>
2025-03-11 16:51:35 +0900

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by 최영건 on 3/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project1Tests/project1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project1Tests.swift
+//  project1Tests
+//
+//  Created by 최영건 on 3/11/25.
+//
+
+import Testing
+@testable import project1
+
+struct project1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
