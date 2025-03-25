6e241406128bef49f183549ad11c05ee259102a8
choiyeonggeon <aws8792@naver.com>
2025-03-25 12:04:13 +0900

    눈물머금은

---
+++ test/1111/1111Tests/_111Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _111Tests.swift
+//  1111Tests
+//
+//  Created by 최영건 on 3/25/25.
+//
+
+import Testing
+@testable import _111
+
+struct _111Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
