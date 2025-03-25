cf6f88427891ecb7d3cba5eedf8a7c24bcb2d3bb
choiyeonggeon <aws8792@naver.com>
2025-03-25 11:39:04 +0900

    test
    test

---
+++ project1/1/1/우정잉30대아줌마/test/testTests/testTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by 최영건 on 3/25/25.
+//
+
+import Testing
+@testable import test
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
--- project1/1/1/우정잉30대아줌마/우정잉30대아줌마/ViewController.swift
+++ project1/1/1/우정잉30대아줌마/우정잉30대아줌마/ViewController.swift
@@ -1,19 +1,19 @@
 //
 //  ViewController.swift
-//  project1
+//  우정잉30대아줌마
 //
-//  Created by 최영건 on 3/11/25.
+//  Created by 최영건 on 3/17/25.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        
+        }
+    @IBOutlet weak var myButton: UIButton!
 }
 
